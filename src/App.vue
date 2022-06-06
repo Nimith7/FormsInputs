2db4834a15bacb8aba344020331b064aeedb56c3
Nicolas Calvi <nicolascalvi.35@gmail.com>
2022-06-06 20:52:25 +0200

    Project Start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script></script>
+
+<template></template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
